365b3ae83adcc486272ae547f3f14d92dd0f4389
Razzi Abuissa <razzi53@gmail.com>
2020-01-31 23:37:55 -0800

    Add simple routing and prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './Home'
+import Game from './Game'
+import Select from './Select'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Link to="/">Insert home logo here</Link>
+        <Switch>
+          <Route path="/select">
+            <Select />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <div>
+    <Link to="/select">Start</Link>
+  </div>
+)

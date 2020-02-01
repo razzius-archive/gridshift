d40192003b4ca86f12e4d11bac2eea93dff6d4dc
Loquen Jones <lrjones416@gmail.com>
2020-02-01 10:53:01 -0800

    add character array and render

---
+++ src/Character.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Character = ({description, image}) => {
+  return (
+    <div>
+      {description}
+    </div>
+  );
+};
+
+export default Character;
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,40 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+import Character from './Character'
+
+let characters = [
+  {
+    description: 'Retired Investment Banker from New York',
+    image: ''
+  },
+  {
+    description: 'Startup Founder who owns a Tesla Cybertruck',
+    image: ''
+  },
+  {
+    description: 'Working Mom with 2 kids living in an appartment',
+    image: ''
+  },
+  {
+    description: 'High School English Teacher who rides public transit',
+    image: ''
+  }
+]
 
 export default () => (
-
+  
 
   <div>
     <h1>California Power Trail</h1>
     <h2>Choose Your Character</h2>
-
+    
+      {characters.map((character, idx) => (
+        <Character
+          key={idx}
+          description={character.description}
+          image={character.image}
+        />
+      ))}
 
     <Link to="/game">Start</Link>
   </div>

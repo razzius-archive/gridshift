e099406a552dfd5c68f49fe9b15ddd3af170567b
Loquen Jones <lrjones416@gmail.com>
2020-02-01 10:42:11 -0800

    remove select and convert app to class

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default () => (
+
+
   <div>
-    <Link to="/select">Start</Link>
+    <h1>California Power Trail</h1>
+    <h2>Choose Your Character</h2>
+
+
+    <Link to="/game">Start</Link>
   </div>
 )
--- src/Select.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export default () => (
-  <div>
-    <Link to="/game">Select something</Link>
-  </div>
-)
